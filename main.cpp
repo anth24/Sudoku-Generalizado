64aa7b021a55ee31da51e5cd219a124ba010d050
anth24 <40000508+anth24@users.noreply.github.com>
2018-11-26 12:28:48 -0500

    Hallo
    Corrección del archivo

--- main.cpp
+++ main.cpp
@@ -1,37 +1,40 @@
 #include <iostream>
-#include <stdlib.h>
-#include <ctime>
-#include <vector>
 #include "Sudoku.h"
+#include <ctime>
+#include <cstdlib>
 
 void presentacion () {
 
-  srand(time(0));
+    srand(time(0));
 
-  std::cout << "Ingrese la opcion:\n" << "1: Sudoku 2x2\n" << "2: Sudoku 3x3\n";
+    std::cout << "Ingrese la opcion:\n" << "1: Sudoku 2x2\n" << "2: Sudoku 3x3\n"  << "3: Sudoku 4x4\n";
 
-  int entrada = 0;
-  bool continuar;
-  do {
-      std::cin >> entrada;
-      if (entrada == 1) {
-          Sudoku(4);
-          std :: cout <<"s4"<<"\n";
-          continuar = false;
-      } else {
-          if (entrada == 2) {
-              std::cout << "s9" << "\n";
-              Sudoku(9);
-              continuar = false;
-          } else {
-              std::cout << "error vuelve a intentar" << "\n";
-              continuar = true;
-          }
-      }
-  }while (continuar);
+    int entrada = 0;
+    bool continuar;
+    do {
+        std::cin >> entrada;
+        if (entrada == 1) {
+            Sudoku(4);
+            continuar = false;
+        } else {
+            if (entrada == 2) {
+                Sudoku(9);
+                continuar = false;
+            } else {
+                if (entrada == 3){
+                    Sudoku(16);
+                    continuar =false;
+                }else {
+                    std::cout << "Error vuelve a intentar" << "\n";
+                    continuar = true;
+                }
+            }
+        }
+    }while (continuar);
 };
 
 int main() {
     presentacion();
+
     return 0;
 }
